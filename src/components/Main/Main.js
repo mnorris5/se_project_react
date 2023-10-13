235e674d1f4e75a44652f9fe114dc80d8a0ae3c2
Mike Norris <minorris513@gmail.com>
2023-10-13 11:04:46 -0400

    refactoring filtered cards function

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,18 +4,20 @@ import ItemCard from "../ItemCard/ItemCard";
 import { useMemo } from "react";
 
 function Main({ weatherTemp, onSelectCard }) {
-  const weatherType = useMemo(() => {
-    if (weatherTemp >= 86) {
-      return "hot";
-    } else if (weatherTemp >= 66 && weatherTemp <= 85) {
-      return "warm";
-    } else if (weatherTemp <= 65) {
-      return "cold";
-    }
-  }, [weatherTemp]);
+  const getWeatherType =
+    (() => {
+      if (weatherTemp >= 86) {
+        return "hot";
+      } else if (weatherTemp >= 66 && weatherTemp <= 85) {
+        return "warm";
+      } else if (weatherTemp <= 65) {
+        return "cold";
+      }
+    },
+    [weatherTemp]);
 
-  const filteredCards = defaultClothingItems.filter((item) => {
-    return item.weather.toLowerCase() === weatherType;
+  const filteredCards = defaultClothingItems.find((item) => {
+    return item.weather.toLowerCase() === getWeatherType;
   });
 
   return (
